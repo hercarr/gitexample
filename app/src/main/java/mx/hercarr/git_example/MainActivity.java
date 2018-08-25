475571ebf7f10618ca3dcf819250b22189d31692
hercarr <hercarr.mx@gmail.com>
2018-08-25 10:49:37 -0500

    feature 3

--- app/src/main/java/mx/hercarr/git_example/MainActivity.java
+++ app/src/main/java/mx/hercarr/git_example/MainActivity.java
@@ -25,5 +25,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
     }
-    
+
 }
